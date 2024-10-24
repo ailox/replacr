48041b4bab9e081e885b364d9b0fe28d8ea756f5
Paul <paul@zom.bi>
2024-10-24 15:21:57 +0200

    Update action to work with paths

--- .github/actions/replace-tag/entrypoint.sh
+++ .github/actions/replace-tag/entrypoint.sh
@@ -1,14 +1,17 @@
 #!/bin/sh
+set -eu
 
 # Arguments: input file, tag 1, tag 2
-file=$1
+path=$1
 old_tag=$2
 new_tag=$3
 
-if [ ! -f "$file" ]; then
-  echo "File not found: $file"
+if [ ! -e "$path" ]; then
+  echo "Path does not exist: $path"
   exit 1
 fi
 
-sed -i "s/\b$old_tag\b/$new_tag/g" "${file}"
-echo "Replaced '${old_tag}' with '${new_tag}' in ${file}"
+find "${path}" -type f -exec \
+    sed -i "s/\\b${old_tag}\\b/${new_tag}/g" {} \;
+
+echo "Replaced all references of '${old_tag}' with '${new_tag}' in '${path}'"
